a7a57be7cc18f096c62ee66aad9e99e94d5a72e1
BocasFx <bocasfx@gmail.com>
2014-10-07 11:12:54 -0400

    Completed first draft

--- Usability/js/usability.js
+++ Usability/js/usability.js
@@ -1,12 +1,12 @@
-$( document ).ready(function() {
-	var play = true;
-	$("body").keyup(function(e){
-		console.log(e.keyCode);
-		if(e.keyCode == 13 && play) {
-			$('.active video').get(0).play();
-		} else {
-			$('.active video').get(0).pause();
-		}
-		play = !play;
-	});
-});
+// $( document ).ready(function() {
+// 	var play = true;
+// 	$("body").keyup(function(e){
+// 		console.log(e.keyCode);
+// 		if(e.keyCode == 13 && play) {
+// 			$('.active video').get(0).play();
+// 		} else {
+// 			$('.active video').get(0).pause();
+// 		}
+// 		play = !play;
+// 	});
+// });
